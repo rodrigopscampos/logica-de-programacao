184ea5faf8511a3180b006b51937387bfc2928d3
Rodrigo Campos <rodrigo.campos@intelitrader.com.br>
2018-09-14 20:27:01 -0300

    Adiciona explicacao para os operadores while e do

---
+++ LogicaDeProgramacao/1_Explicacoes/6_OperadorWhile.cs
@@ -0,0 +1,32 @@
+﻿using LogicaDeProgramacao.Explicacoes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicaDeProgramacao.Explicacoes
+{
+    public class OperadorWhile : IExplicacao
+    {
+        public void Explicar()
+        {
+            Console.WriteLine(@"
+    A instrução while executa uma instrução ou um bloco de instruções enquanto uma expressão booliana especificada é avaliada como true
+
+    Exemplo:
+
+        int n = 0;
+        while (n < 3)
+        {
+            Console.WriteLine('n = ' + n);
+            n++;
+        }
+
+        //n = 0
+        //n = 1
+        //n = 2
+");
+        }
+    }
+}
---
+++ LogicaDeProgramacao/1_Explicacoes/7_OpeardorDo.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace LogicaDeProgramacao.Explicacoes
+{
+    public class OpeardorDo : IExplicacao
+    {
+        public void Explicar()
+        {
+            Console.WriteLine(@"
+    A instrução do executa uma instrução ou um bloco de instruções enquanto uma expressão booliana especificada é avaliada como true.
+
+    Exemplo:
+        int n = 0;
+        do 
+        {
+            Console.WriteLine('n = ' + n);
+            n++;
+        } while (n < 3);
+
+        //n = 0
+        //n = 1
+        //n = 2
+");
+        }
+    }
+}
