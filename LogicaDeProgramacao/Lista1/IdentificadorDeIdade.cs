d295de4c92bc475c8b159acc8b857b251fa0d950
Rodrigo Campos <rodigopscampos@gmail.com>
2018-11-27 18:41:20 -0200

    Ajusta cenários de teste do exercicio Identificador de idade

--- LogicaDeProgramacao/Lista1/IdentificadorDeIdade.cs
+++ LogicaDeProgramacao/Lista1/IdentificadorDeIdade.cs
@@ -16,10 +16,11 @@ namespace LogicaDeProgramacao.Lista1
         public bool VerificarResposta()
         {
             return
-                   Validar.SaoIguais("Adulto", () => Rodar(19))
-                && Validar.SaoIguais("Juvenil", () => Rodar(18))
+                   Validar.SaoIguais("Infantil", () => Rodar(0))
                 && Validar.SaoIguais("Infantil", () => Rodar(10))
-                && Validar.SaoIguais("Infantiil", () => Rodar(0))
+                && Validar.SaoIguais("Infanto", () => Rodar(15))
+                && Validar.SaoIguais("Juvenil", () => Rodar(18))
+                && Validar.SaoIguais("Adulto", () => Rodar(19))
                 && Validar.SaoIguais("Adulto", () => Rodar(60));
         }
 
